a4bd20be2e3dc07294c1c40c1d5fa2bfe05e2800
Tang <ricenoddlet@gmail.com>
2020-07-02 10:01:09 -0400

    Set Firebase
    initialize app with AngularFireModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,19 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { environment } from '../environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFireModule } from '@angular/fire';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig: {
+    apiKey: 'AIzaSyBE5AgvpM-cu0Oy3FeJ-nnui2FRj6JXj8s',
+    authDomain: 'pets-ground.firebaseapp.com',
+    databaseURL: 'https://pets-ground.firebaseio.com',
+    projectId: 'pets-ground',
+    storageBucket: 'pets-ground.appspot.com',
+    messagingSenderId: '43391279763',
+    appId: '1:43391279763:web:12fe7e7e2d9a239b75a366',
+  },
 };
 
 /*
